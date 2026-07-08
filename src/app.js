7cc5e689f3f79e67ef9a0b89b9a752f22c130fac
elidistro7 <elidistro@gmail.com>
2026-07-08 18:00:44 +0300

    new ways12

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.use(cors({
   },
   methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
+  credentials: false,
 }));
 
 app.use(rateLimit({
