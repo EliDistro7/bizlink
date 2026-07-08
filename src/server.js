54b87ab8de7dee3689c0295e2185eb73581958b1
elidistro7 <elidistro@gmail.com>
2026-07-08 14:21:08 +0300

    new ways4

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 require('express-async-errors');
 
-const app = require('./src/app');
-const connectDB = require('./config/database');
+const app = require('./app');
+const connectDB = require('./config/db');
 const logger = require('./config/logger');
 
 const PORT = process.env.PORT || 5000;
